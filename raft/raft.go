10d07ad0904c00bc0a545d4f57ec30dd982e6501
bigboss2063 <bigboss2063@outlook.com>
2022-11-02 15:48:59 +0800

    feat: Pass tests of project3a.

--- raft/raft.go
+++ raft/raft.go
@@ -301,7 +301,10 @@ func (r *Raft) broadcastHeartbeat() {
 // current commit index to the given peer. Returns true if a message was sent.
 func (r *Raft) sendAppend(to uint64) bool {
 	// Your Code Here (2A).
-	pr := r.Prs[to]
+	pr, ok := r.Prs[to]
+	if !ok {
+		return false
+	}
 	li := r.RaftLog.LastIndex()
 	var prevLogIndex uint64
 	if pr.Next-1 > li {
@@ -370,7 +373,7 @@ func (r *Raft) tick() {
 
 func (r *Raft) electionTicker() {
 	r.electionElapsed++
-	if r.electionElapsed >= r.randomizedElectionTimeout {
+	if r.promotable() && r.electionElapsed >= r.randomizedElectionTimeout {
 		r.electionElapsed = 0
 		r.Step(pb.Message{MsgType: pb.MessageType_MsgHup, To: r.id, From: r.id})
 	}
@@ -382,24 +385,39 @@ func (r *Raft) heartbeatTicker() {
 
 	if r.electionElapsed >= r.randomizedElectionTimeout {
 		r.electionElapsed = 0
+		if r.State == StateLeader && r.leadTransferee != None {
+			r.abortLeaderTransfer()
+		}
 	}
-
+	if r.State != StateLeader {
+		return
+	}
 	if r.heartbeatElapsed >= r.heartbeatTimeout {
 		r.heartbeatElapsed = 0
 		r.Step(pb.Message{MsgType: pb.MessageType_MsgBeat, From: r.id, To: r.id})
 	}
 }
 
+func (r *Raft) reset(term uint64) {
+	if r.Term != term {
+		r.Term = term
+		r.Vote = None
+	}
+	r.Lead = None
+	r.electionElapsed = 0
+	r.heartbeatElapsed = 0
+	r.resetRandomizedElectionTimeout()
+	r.abortLeaderTransfer()
+	r.votes = make(map[uint64]bool)
+	r.PendingConfIndex = None
+}
+
 // becomeFollower transform this peer's state to Follower
 func (r *Raft) becomeFollower(term uint64, lead uint64) {
 	// Your Code Here (2A).
-	r.State = StateFollower
-	r.Term = term
+	r.reset(term)
 	r.Lead = lead
-	r.Vote = None
-	r.votes = make(map[uint64]bool)
-	r.electionElapsed = 0
-	r.resetRandomizedElectionTimeout()
+	r.State = StateFollower
 }
 
 // becomeCandidate transform this peer's state to candidate
@@ -408,13 +426,10 @@ func (r *Raft) becomeCandidate() {
 	if r.State == StateLeader {
 		log.Panicf("%v can not become candidate from leader", r.id)
 	}
+	r.reset(r.Term + 1)
 	r.State = StateCandidate
-	r.Term = r.Term + 1
 	r.Vote = r.id
-	r.votes = make(map[uint64]bool)
 	r.votes[r.id] = true
-	r.electionElapsed = 0
-	r.resetRandomizedElectionTimeout()
 }
 
 // becomeLeader transform this peer's state to leader
@@ -424,12 +439,20 @@ func (r *Raft) becomeLeader() {
 	if r.State == StateFollower {
 		log.Panicf("%v can not become candidate from leader", r.id)
 	}
+	r.reset(r.Term)
 	r.State = StateLeader
 	r.Lead = r.id
-	r.heartbeatElapsed = 0
 	for i := range r.Prs {
 		r.Prs[i].Match = 0
 		r.Prs[i].Next = r.RaftLog.LastIndex() + 1
+	}
+	ents, err := r.RaftLog.Entries(r.RaftLog.applied+1, r.RaftLog.committed+1)
+	if err != nil {
+		log.Panicf("unexpected error getting uncommitted entries (%v)", err)
+	}
+	pendingConfIndex := numOfPendingConf(ents)
+	if pendingConfIndex != None {
+		r.PendingConfIndex = pendingConfIndex
 	}
 	r.RaftLog.append(&pb.Entry{Index: r.RaftLog.LastIndex() + 1, Term: r.Term})
 	// 为什么要更新自己的 progress ？不更新过不了测试
@@ -471,7 +494,7 @@ func (r *Raft) Step(m pb.Message) error {
 			r.msgs = append(r.msgs, pb.Message{MsgType: pb.MessageType_MsgRequestVoteResponse, From: r.id, To: m.From, Reject: true, Term: r.Term})
 		case pb.MessageType_MsgAppend:
 			r.msgs = append(r.msgs, pb.Message{MsgType: pb.MessageType_MsgAppendResponse, From: r.id, To: m.From, Term: r.Term})
-		case pb.MessageType_MsgHeartbeat:
+		case pb.MessageType_MsgHeartbeat, pb.MessageType_MsgSnapshot:
 			r.msgs = append(r.msgs, pb.Message{MsgType: pb.MessageType_MsgHeartbeatResponse, From: r.id, To: m.From, Term: r.Term})
 		}
 	}
@@ -482,6 +505,15 @@ func (r *Raft) Step(m pb.Message) error {
 		DPrintf("hup message: %v", m)
 		if r.State == StateLeader {
 			DPrintf("%v already became a leader", r.id)
+			return nil
+		}
+		// 找出还没有应用但已经提交的 conf change
+		ents, err := r.RaftLog.Entries(r.RaftLog.applied+1, r.RaftLog.committed+1)
+		if err != nil {
+			log.Panicf("unexpected error getting unapplied entries (%v)", err)
+		}
+		if index := numOfPendingConf(ents); index != 0 && r.RaftLog.committed > r.RaftLog.applied {
+			DPrintf("%v can't begin a election, because there is a conf change entry wait to apply ,index %v", r.id, index)
 			return nil
 		}
 		r.becomeCandidate()
@@ -647,11 +679,59 @@ func (r *Raft) restore(s *pb.Snapshot) bool {
 // addNode add a new node to raft group
 func (r *Raft) addNode(id uint64) {
 	// Your Code Here (3A).
+	r.PendingConfIndex = None
+	// 不能添加重复的节点
+	if _, ok := r.Prs[id]; ok {
+		return
+	}
+	r.Prs[id] = &Progress{
+		Match: 0,
+		Next:  r.RaftLog.LastIndex() + 1,
+	}
 }
 
 // removeNode remove a node from raft group
 func (r *Raft) removeNode(id uint64) {
 	// Your Code Here (3A).
+	delete(r.Prs, id)
+	r.PendingConfIndex = None
+	if len(r.Prs) == 0 {
+		return
+	}
+	// 现在节点变少了，那么可能达到提交的要求（一半的节点已经复制日志）了，尝试一下提交
+	r.maybeCommit()
+	// 如果被移除的节点是 leadTransferee，且当前节点是 leader，终止 leader transfer
+	if r.State == StateLeader && id == r.leadTransferee {
+		r.abortLeaderTransfer()
+	}
+}
+
+func (r *Raft) abortLeaderTransfer() {
+	r.leadTransferee = None
+}
+
+func (r *Raft) promotable() bool {
+	_, ok := r.Prs[r.id]
+	return ok
+}
+
+func (r *Raft) maybeCommit() {
+	savedCommitted := r.RaftLog.committed
+	for N := r.RaftLog.LastIndex(); N >= r.RaftLog.FirstIndex(); N-- {
+		matchCount := 1
+		for i := range r.Prs {
+			if r.Prs[i].Match >= N && i != r.id {
+				matchCount++
+			}
+		}
+		term, _ := r.RaftLog.Term(N)
+		if matchCount > len(r.Prs)/2 && term == r.Term && N > savedCommitted {
+			r.RaftLog.commitTo(N)
+			DPrintf("%v commit to %v at term %v, logs %v %v", r.id, N, r.Term, r.RaftLog.entries, r.RaftLog.committed)
+			r.broadcastAppend()
+			break
+		}
+	}
 }
 
 func tickLeader(r *Raft, m pb.Message) {
@@ -662,6 +742,20 @@ func tickLeader(r *Raft, m pb.Message) {
 		DPrintf("%v receive propose message: %v", r.id, m)
 		if len(m.Entries) == 0 {
 			log.Panicf("%v can't append nil entries", r.id)
+		}
+		// 如果当前正在进行 leadTransfer，就不再接收新的日志
+		if r.leadTransferee != None {
+			DPrintf("%v leadTransferee is not None %v, stopping propose logs", r.id, r.leadTransferee)
+		}
+		for i, e := range m.Entries {
+			if e.EntryType == pb.EntryType_EntryConfChange {
+				// 如果还有未 apply 的 conf change，就无视新的 conf change
+				if r.PendingConfIndex != None {
+					DPrintf("%v ignore conf change entry", r.id)
+					m.Entries[i] = &pb.Entry{EntryType: pb.EntryType_EntryNormal}
+				}
+				r.PendingConfIndex = m.Entries[i].Index
+			}
 		}
 		li := r.RaftLog.LastIndex()
 		for i := range m.Entries {
@@ -682,11 +776,15 @@ func tickLeader(r *Raft, m pb.Message) {
 		}
 	case pb.MessageType_MsgAppendResponse:
 		DPrintf("%v receive append response msg %v", r.id, m)
+		pr, ok := r.Prs[m.From]
+		if !ok {
+			return
+		}
 		switch m.Reject {
 		case true:
 			// 如果 logTerm 等于 0 表示 follower 在 pervLogIndex 位置没有日志，将 next 设置为 follower 的 lastIndex + 1
 			if m.LogTerm == 0 {
-				r.Prs[m.From].Next = m.Index
+				pr.Next = m.Index
 			} else {
 				lastIndexOfTerm := uint64(0)
 				for i := r.RaftLog.LastIndex(); i >= r.RaftLog.FirstIndex(); i-- {
@@ -697,32 +795,46 @@ func tickLeader(r *Raft, m pb.Message) {
 				}
 				// leader 有冲突任期的日志，那么把冲突任期后面任期的日志都发过去
 				if lastIndexOfTerm > 0 {
-					r.Prs[m.From].Next = lastIndexOfTerm + 1
+					pr.Next = lastIndexOfTerm + 1
 				} else {
 					// 否则将包括 follower 冲突任期的第一条日志所在的索引位置和之后的日志全部发过去
-					r.Prs[m.From].Next = m.Index
+					pr.Next = m.Index
 				}
 			}
 			r.sendAppend(m.From)
 		case false:
-			r.Prs[m.From].Match = m.Index
-			r.Prs[m.From].Next = r.Prs[m.From].Match + 1
-			savedCommitted := r.RaftLog.committed
-			for N := r.RaftLog.LastIndex(); N >= r.RaftLog.FirstIndex(); N-- {
-				matchCount := 1
-				for i := range r.Prs {
-					if r.Prs[i].Match >= N && i != r.id {
-						matchCount++
-					}
-				}
-				term, _ := r.RaftLog.Term(N)
-				if matchCount > len(r.Prs)/2 && term == r.Term && N > savedCommitted {
-					r.RaftLog.commitTo(N)
-					DPrintf("%v commit to %v at term %v, logs %v %v", r.id, N, r.Term, r.RaftLog.entries, savedCommitted)
-					r.broadcastAppend()
-					break
-				}
+			pr.Match = m.Index
+			pr.Next = r.Prs[m.From].Match + 1
+			r.maybeCommit()
+			// 如果这条消息是由 leadTransferee 发送过来的，并且日志已经同步到最新，那么就可以通知 leadTransferee 开始转移
+			if m.From == r.leadTransferee && pr.Match == r.RaftLog.LastIndex() {
+				r.msgs = append(r.msgs, pb.Message{MsgType: pb.MessageType_MsgTimeoutNow, To: m.From, From: r.id, Term: r.Term})
 			}
+		}
+	case pb.MessageType_MsgTransferLeader:
+		leaderTransferee := m.From
+		lastLeadTransferee := r.leadTransferee
+		if lastLeadTransferee != None {
+			if lastLeadTransferee == leaderTransferee {
+				DPrintf("leaderTransfer already begin, leaderTransferee %v", leaderTransferee)
+				return
+			}
+			// 出现了新的 leaderTransferee，就把老的结束
+			r.abortLeaderTransfer()
+		}
+		if leaderTransferee == r.id {
+			DPrintf("leaderTransferee %v already been a leader", leaderTransferee)
+			return
+		}
+		// 转移要在一个选举间隔内完成，重置选举时间
+		r.electionElapsed = 0
+		r.leadTransferee = leaderTransferee
+		// leaderTransferee 的日志已经是最新的了，直接通知它转移
+		if pr, ok := r.Prs[leaderTransferee]; ok && pr.Match == r.RaftLog.LastIndex() {
+			r.msgs = append(r.msgs, pb.Message{MsgType: pb.MessageType_MsgTimeoutNow, To: leaderTransferee, From: r.id, Term: r.Term})
+		} else {
+			// 否则帮助它更新到最新
+			r.sendAppend(leaderTransferee)
 		}
 	}
 }
@@ -760,11 +872,20 @@ func tickCandidate(r *Raft, m pb.Message) {
 		DPrintf("%v receive snapshot message: %v", r.id, m)
 		r.becomeFollower(m.Term, m.From)
 		r.handleSnapshot(m)
+	case pb.MessageType_MsgTimeoutNow:
+		DPrintf("%v is a candidate and ignored time message from %v", r.id, m.From)
 	}
 }
 
 func tickFollower(r *Raft, m pb.Message) {
 	switch m.MsgType {
+	case pb.MessageType_MsgPropose:
+		if r.Lead == None {
+			return
+		}
+		m.To = r.Lead
+		r.msgs = append(r.msgs, m)
+		DPrintf("%v redirect propose msg to lead %v", r.id, r.Lead)
 	case pb.MessageType_MsgAppend:
 		DPrintf("%v receive append entries message: %v", r.id, m)
 		r.Lead = m.From
@@ -779,5 +900,34 @@ func tickFollower(r *Raft, m pb.Message) {
 		r.Lead = m.From
 		r.electionElapsed = 0
 		r.handleSnapshot(m)
+	case pb.MessageType_MsgTimeoutNow:
+		DPrintf("%v receive timeout message: %v", r.id, m)
+		if r.promotable() {
+			// 还在集群中才能发起选举
+			DPrintf("%v can begin a new election", r.id)
+			r.Step(pb.Message{MsgType: pb.MessageType_MsgHup, To: r.id, From: r.id})
+			return
+		}
+		DPrintf("%v already been removed", r.id)
+	case pb.MessageType_MsgTransferLeader:
+		if r.Lead == None {
+			DPrintf("%v don't have a leader", r.id)
+			return
+		}
+		// 给 leader 转发消息
+		m.To = r.Lead
+		m.Term = r.Term
+		r.msgs = append(r.msgs, m)
+		DPrintf("%v redirect transfer leader msg to leader %v", r.id, r.Lead)
 	}
+}
+
+func numOfPendingConf(ents []*pb.Entry) uint64 {
+	pendingConfIndex := None
+	for i := range ents {
+		if ents[i].EntryType == pb.EntryType_EntryConfChange {
+			pendingConfIndex = ents[i].Index
+		}
+	}
+	return pendingConfIndex
 }
